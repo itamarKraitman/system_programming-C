15a4d4f35ce39bc72b3384858adeb647350f2d29
itamarKraitman <itamar.kraitman@msmail.ariel.ac.il>
2021-11-02 14:52:37 +0200

    firs commit

---
+++ ex1/basicClassification.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+//return if an integer is prime
+int isPrime(int numberToCheck) {
+    for(int i = 2; i <= sqrt(numberToCheck); i++) {
+            if((numberToCheck % i) == 0) {
+                return 0;
+         }
+    }
+    return 1;
+}
+
+//return if an integer is strong
+int isStrong(int numberToCheck) {
+    int sum = 0;
+    int number = numberToCheck
+    while(number > 9) {
+        sum += factorial(number % 10);
+        number = number / 10;
+    }
+    sum += factorial(number);
+    if(sum == number) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+//computing factorial
+int factorial(int number) {
+    if(number == 1) {
+        return 1;
+    }
+    return  factorial(number - 1) * number;
+}
+
+
---
+++ ex1/numClass.h
@@ -0,0 +1,3 @@
+int isPrime(int numberToCheck);
+int isStrong(int numberToCheck);
+int factorial(int number);

a63e14aa622e4f29f1629a71c8c740d093a67c6d
itamarKraitman <itamar.kraitman@msmail.ariel.ac.il>
2021-11-02 15:58:14 +0200

    start second file

--- ex1/advancedClassificationLoop.c
+++ ex1/advancedClassificationLoop.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+#include <math.h>
 
+//checking if a number is palindrom
 int isPalindrom(int numberToCheck) {
-    
+    int reversedNumber = 1, number = numberToCheck;
+    while (number != 0) {
+        int remained = number % 10;
+        reversedNumber *= 10 + remained;
+        number /= 10;
+    }
+    if(reversedNumber == numberToCheck) {
+        return 1;
+    }
+    return 0;
 }
